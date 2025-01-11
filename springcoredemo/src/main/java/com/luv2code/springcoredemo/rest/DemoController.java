85fa5961fdc13ad4c6eb65665730210788636a3a
PreciousMotloung <preciousmotloung01@gmail.com>
2025-01-11 12:04:49 +0200

    Spring bean demonstration
    Spring bean demonstration

--- springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -11,18 +11,27 @@ public class DemoController {
 
     // define a private field for the dependency
     private Coach myCoach;
+    private Coach anotherCoach;
 
     // define a constructor for dependency injection
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach)
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach,
+                          @Qualifier("cricketCoach") Coach anotherCoach)
     {
         System.out.println("In constructor" + getClass().getName());
         this.myCoach = theCoach;
+        this.anotherCoach = anotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkouts()
     {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check()
+    {
+        return "comparing beans: myCoach = anotherCoach, "+(myCoach == anotherCoach);
     }
 }
