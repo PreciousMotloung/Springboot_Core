db3a4d6a9b52c36ce3f01f7d8205cfe10acd6313
PreciousMotloung <preciousmotloung01@gmail.com>
2025-01-11 11:29:36 +0200

    springboot lazy initialization demonsration

--- springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -1,6 +1,8 @@
-package com.luv2code.springcoredemo;
+package com.luv2code.springcoredemo.rest;
 
+import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,9 @@ public class DemoController {
 
     // define a constructor for dependency injection
     @Autowired
-    public DemoController(Coach theCoach)
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach)
     {
+        System.out.println("In constructor" + getClass().getName());
         this.myCoach = theCoach;
     }
 
